89922bd07a68698d126fc70d82c9de1dfa48ca87
Brian Cavagnolo <brian@cozybit.com>
2010-10-12 17:20:37 -0700

    update wtfconfig file for the mwl8k

--- platform/mwl8k/wtfconfig.py
+++ platform/mwl8k/wtfconfig.py
@@ -2,17 +2,17 @@ import wtf.node.ap
 import wtf.node.sta
 import wtf.comm
 
-# create AP configurations that match the APs in your vicinity
-ap_serial = wtf.comm.Serial(port="/dev/ttyUSB1",
-                            prompt="[root@localhost dev]# ")
+# create AP configuration for the mwl8k
+ap_serial = wtf.comm.Serial(port="/dev/ttyUSB0", prompt="~ # ")
 ap_serial.name = "AP"
-ap_serial.verbosity = 1
-ap = wtf.node.ap.Hostapd(ap_serial, "libertas_tf_sdio", "wlan0")
+ap_serial.verbosity = 2
+ap = wtf.node.ap.Hostapd(ap_serial, "wlan0")
 
-sta_ssh = wtf.comm.SSH(ipaddr="192.168.1.61")
+# create a configuration for a nearby STA
+sta_ssh = wtf.comm.SSH(ipaddr="192.168.1.60")
 sta_ssh.name = "STA"
 sta_ssh.verbosity = 1
-sta = wtf.node.sta.LinuxSTA(sta_ssh, "libertas_tf_sdio", "wlan0")
+sta = wtf.node.sta.LinuxSTA(sta_ssh, "wlan0", driver="libertas_tf_sdio")
 
 # tell wtf about all of your nodes
 nodes = [ ap, sta ]
