4aca1600e044aca269ec75e8b3946a677635c0fb
Thomas Pedersen <thomas@cozybit.com>
2012-10-17 12:50:29 -0700

    mcca: start traffic and capture
    Still need to get the capture back for analysis somehow.

--- platform/zotac/wtfconfig.py
+++ platform/zotac/wtfconfig.py
@@ -10,7 +10,7 @@ for n in range(8,12):
     z_ssh.name = "zotac-" + str(n)
     z_ssh.verbosity = 2
 # XXX: giving a MAC is robust
-    z = wtf.node.mesh.MeshSTA(z_ssh, "wlan2")
+    z = wtf.node.mesh.MeshSTA(z_ssh, iface="wlan2", driver="ath9k_htc")
     z.config = wtf.node.mesh.MeshConf(ssid="meshpoo", channel=1)
     z.ip = "192.168.33." + str(10 + n)
     zotacs.append(z)
--- tests/mcca.py
+++ tests/mcca.py
@@ -2,7 +2,34 @@
 # All rights reserved
 
 """
-Test mesh MCCA deliverables as per the SoW
+Test mesh MCCA deliverables as per the SoW:
+
+	Test 1:
+	-------
+		STA1 and STA2 with MCCA enabled
+		Generate continuous mcast and unicast traffic at each STA.
+		PASS: traffic is transmitted only during alloted intervals
+
+	Test 2:
+	-------
+		Add STA3 w/ MCCA on and STA4 w/ MCCA off
+		Generate continuous mcast and unicast traffic at each STA.
+		PASS: traffic is transmitted only during alloted intervals,
+		except from STA4 who has no MCCA knowledge
+
+	Test 3:
+	-------
+		Enable MCCA at STA4. Generate traffic and verify traffic is
+		transmitted only during allotted intervals.
+
+	Test 4:
+	-------
+		Disable MCCA reservations at STA1 and STA2, they should
+		transmit whenever they please, while STA3 and STA4 still
+		respects each other's MCCAOPs
+
+PASS CRITERIA:
+	Peer MCCAOPs are respected
 """
 
 import wtf.node.mesh as mesh
@@ -13,22 +40,31 @@ import wtf
 wtfconfig = wtf.conf
 sta = wtfconfig.mps
 
+# global setup, called once during this suite
+def setUp(self):
+# start with just STA1 and 2
+    for n in wtfconfig.nodes[:2]:
+        n.shutdown()
+        n.init()
+
 class TestMCCA(unittest.TestCase):
 
-# setUp and tearDown are called by nose before / after each test
+# setUp and tearDown are called by nose before / after each test, but all tests
+# are a continuation of eachother, so do nothing between tests
     def setUp(self):
-        for n in wtfconfig.nodes:
-            n.shutdown()
-            n.init()
+        pass
 
-    def test_init_shutdown(self):
-        for n in wtfconfig.nodes:
-            n.init()
-            n.shutdown()
-            n.init()
-            n.shutdown()
+    def test_1(self):
+# start capturing
+        sta[0].start_capture()
+# install reservations
+        sta[0].set_mcca_res(sta[1])
+        sta[1].set_mcca_res(sta[0])
+# send traffic
+        sta[0].perf()
+        sta[1].perf(sta[0].ip, timeout=10)
 
-    def test_ping(self):
-        self.failIf(sta[0].ping(sta[1].ip, timeout=5) != 0,
-                    "failed to ping sta1 at" + sta[1].ip)
-
+        sta[0].killperf()
+        sta[1].killperf()
+        sta[0].stop_capture()
+# stop capturing
