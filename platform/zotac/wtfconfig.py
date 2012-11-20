e484e15bb90393ae0eb658665c384749fa6f916d
Thomas Pedersen <thomas@cozybit.com>
2012-11-20 14:50:04 -0800

    mcca: use HT20 by default, the MCCA scores are still decent (> 10)

--- platform/zotac/wtfconfig.py
+++ platform/zotac/wtfconfig.py
@@ -12,7 +12,7 @@ for n in range(8,12):
     z_ssh.verbosity = 2
 # XXX: giving a MAC is robust
     z = wtf.node.mesh.MeshSTA(z_ssh, iface="wlan0", driver="ath9k")
-    z.config = wtf.node.mesh.MeshConf(ssid="meshpoo", channel=108, htmode="")
+    z.config = wtf.node.mesh.MeshConf(ssid="meshpoo", channel=108, htmode="HT20")
     z.ip = "192.168.34." + str(10 + n)
     zotacs.append(z)
 
