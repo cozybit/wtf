e5be94b22c44633d59b6cb4ed1fa2ba1f3a9f3f0
Thomas Pedersen <thomas@cozybit.com>
2013-03-05 17:16:28 -0800

    mmultichan: test simultaneous single-hop links
    This tests the topology A -> B -> C, where B has 2
    interfaces on different channels matching A and C.
    
    In this test "a" is the throughput from A -> B, and "b" is
    the throughput from B -> C.

--- tests/mmultichan.py
+++ tests/mmultichan.py
@@ -51,7 +51,7 @@ class TestMMBSS(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_0_baseline(self):
+    def test_0_single_hop(self):
         fname = sys._getframe().f_code.co_name
 
         dst_ip = sta[1].configs[0].iface.ip
@@ -59,7 +59,34 @@ class TestMMBSS(unittest.TestCase):
 
         results[fname] = LinkReport(perf_report=perf_report)
 
-    def test_1_bridge(self):
+    def test_1_dual_single_hop(self):
+        fname = sys._getframe().f_code.co_name
+
+# separate one of the interfaces at IP layer
+        subnet = "192.168.22."
+        old_ip1 = sta[1].configs[1].iface.ip
+        old_ip2 = sta[2].configs[0].iface.ip
+        dst_1 = sta[1].configs[0].iface.ip
+        dst_2 = subnet + "2"
+        sta[1].configs[1].iface.ip = subnet + "1"
+        sta[2].configs[0].iface.ip = dst_2
+        sta[1].reconf()
+        sta[2].reconf()
+
+        sta[1].perf_serve(dst_ip=dst_1)
+        sta[2].perf_serve(dst_ip=dst_2)
+        sta[0].perf_client(dst_ip=dst_1, timeout=10, b=100, fork=True)
+        sta[1].perf_client(dst_ip=dst_2, timeout=10, b=100)
+
+        perf_report = sta[1].get_perf_report()
+        results[fname + "_a"] = LinkReport(perf_report=perf_report)
+        perf_report = sta[2].get_perf_report()
+        results[fname + "_b"] = LinkReport(perf_report=perf_report)
+
+        sta[1].configs[1].iface.ip = old_ip1
+        sta[2].configs[0].iface.ip = old_ip2
+
+    def test_2_bridge(self):
         fname = sys._getframe().f_code.co_name
         dst_ip = sta[2].configs[0].iface.ip
 
@@ -70,13 +97,13 @@ class TestMMBSS(unittest.TestCase):
         perf_report = do_perf([sta[0], sta[2]], dst_ip)
         results[fname] = LinkReport(perf_report=perf_report)
 
-    def test_2_mmbss(self):
+    def test_3_mmbss(self):
         fname = sys._getframe().f_code.co_name
         dst_ip = sta[2].configs[0].iface.ip
 
         # enable in-kernel intra-vif forwarding
-        conf = sta[1].configs[0].shared = True
-        conf = sta[1].configs[1].shared = True
+        sta[1].configs[0].shared = True
+        sta[1].configs[1].shared = True
         sta[1].reconf()
 
         perf_report = do_perf([sta[0], sta[2]], dst_ip)
