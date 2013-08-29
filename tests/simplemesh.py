bfc796a36146443306d437fc15a168124e0cdae2
Jason Abele <jason@cozybit.com>
2013-08-29 16:31:13 -0700

    simplemesh: add 1 channel, 2 hop test
    Borrow the two hop test step from multi-channel test suite for use on the
    nightly zotac simplemesh test.
    
    Signed-off-by: Jason Abele <jason@boo>

--- tests/simplemesh.py
+++ tests/simplemesh.py
@@ -24,13 +24,15 @@ results={}
 # global setup, called once during this suite
 def setUp(self):
 
-# if_1 <-> if_2
+# if_1 <-> if_2 <-> if_3
     global if_1
     global if_2
+    global if_3
 
 
     if_1 = sta[0].iface[0]
     if_2 = sta[1].iface[0]
+    if_3 = sta[2].iface[0]
 
     for n in wtfconfig.mps:
         n.shutdown()
@@ -62,3 +64,22 @@ class SimpleMeshTest(unittest.TestCase):
         perf_report = do_perf([if_1, if_2], dst_ip)
 
         results[fname] = LinkReport(perf_report=perf_report)
+
+    def test_2_same_ch_mhop(self):
+        fname = sys._getframe().f_code.co_name
+
+        # do a -> b -> c
+        ifs = [if_1, if_2, if_3]
+        for iface in ifs:
+            if iface != if_2:
+                iface.conf.mesh_params = "mesh_auto_open_plinks=0"
+            iface.conf.channel = 1
+            iface.node.reconf()
+
+        if_1.add_mesh_peer(if_2)
+        if_3.add_mesh_peer(if_2)
+
+        perf_report = do_perf([if_1, if_3], if_3.ip)
+        # test multi-hop performance using a single radio for forwarding
+        results[fname] = LinkReport(perf_report=perf_report)
+        if_2.dump_mesh_stats()
