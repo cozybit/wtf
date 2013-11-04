1cb0199b06e87c94b378136e5ad9c858e539a4ef
Jacob Minshall <jacob@cozybit.com>
2013-11-03 22:39:19 -0800

    debug statements in the path healing test
    check with gtabs with debugfs radio kill patch to see if this works
    also changed a couple lines at the top of simplemesh, unused imports
    and the doc string to no longer say zotacs

--- tests/simplemesh.py
+++ tests/simplemesh.py
@@ -2,16 +2,13 @@
 # All rights reserved
 
 """
-Simple test of throughput from one zotac to another.
+Simple test of throughput from one mesh node to another.
 """
 
-import wtf.node.mesh as mesh
 import unittest
-import time
 import wtf
 from wtf.util import *
 import sys; err = sys.stderr
-import time
 import os
 
 wtfconfig = wtf.conf
@@ -20,6 +17,7 @@ sta = wtfconfig.mps
 ref_clip = os.getenv("REF_CLIP")
 # XXX: nose probably has something like this?
 results={}
+path_heal_time = {}
 
 # global setup, called once during this suite
 def setUp(self):
@@ -28,7 +26,6 @@ def setUp(self):
     global if_1
     global if_2
     global if_3
-
 
     if_1 = sta[0].iface[0]
     if_2 = sta[1].iface[0]
@@ -45,6 +42,7 @@ def tearDown(self):
 
     print "                                                     ref_clip=%s" % (ref_clip,)
     print_linkreports(results)
+    print "Path healed in:        " + path_heal_time["path_heal"]
 
 class SimpleMeshTest(unittest.TestCase):
 
@@ -83,3 +81,43 @@ class SimpleMeshTest(unittest.TestCase):
         # test multi-hop performance using a single radio for forwarding
         results[fname] = LinkReport(perf_report=perf_report)
         if_2.dump_mesh_stats()
+
+    def test_3_path_healing(self):
+        """Kill a radio, then bring back and measure path heal time."""
+        count = 100
+        interval = .1
+
+        # check if ping is alive
+        _, ping_results = if_1.node.ping(if_2.ip, count=3)
+        ping_results = ping_results.strip("\n")[-1]
+        self.failIf(ping_results.find("100%"), "not connected on initial ping")
+
+        # turn off radio and ping to make sure
+        if_2.set_radio(0)
+        _, ping_results = if_1.node.ping(if_2.ip, count=3)
+        print "debug: ", ping_results
+        ping_results = ping_results.strip("\n")[-1]
+        print "debug: ", ping_results
+        self.failIf(not ping_results.find("100%"),
+                    "not connected on initial ping")
+
+        # turn back on radio and start ping
+        if_2.set_radio(1)
+        _, ping_results = if_1.node.ping(if_2.ip,
+                                         count=count, interval=interval)
+        print "debug: ", ping_results
+        ping_results = ping_results.strip("\n")[-1]
+        print "debug: ", ping_results
+        #the fifth is the packet loss hopefully
+        ping_results = ping_results.strip(" ")[5]
+        print "debug: ", ping_results
+        #take off the precent sign
+        ping_results = ping_results[0:-1]
+        print "debug: ", ping_results
+        self.failIf(int(ping_results) == 100,
+                    "Never reconnected after %d seconds" % (count * interval))
+
+        #expects no loss after reconnected
+        path_heal_time["path_heal"] = (count * interval) - \
+            (count * interval) * (ping_results / 100)
+        print "debug: ", path_heal_time["path_heal"]
