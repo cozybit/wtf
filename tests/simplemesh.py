fb61bc2bec19e8b1152ee13dc2546c7b067fe7ef
Jacob Minshall <jacob@cozybit.com>
2013-11-26 16:57:20 -0800

    use global interface object and init only nodes needed for tests
    topology graph showed extra unused nodes hanging around during tests
    they were not needed for. This calls init_nodes method at beginning of
    each test so only the nodes for the test will be up. It also restores
    the mesh_params so subsequent tests do not auto block peer links

--- tests/simplemesh.py
+++ tests/simplemesh.py
@@ -29,20 +29,10 @@ if len(wtfconfig.exp_results) > 0:
 
 # global setup, called once during this suite
 def setUp(self):
+    """Make global interface variable out of all sta iface[0]."""
+    global g_ifs
 
-# if_1 <-> if_2 <-> if_3
-    global if_1
-    global if_2
-    global if_3
-
-    if_1 = sta[0].iface[0]
-    if_2 = sta[1].iface[0]
-    if_3 = sta[2].iface[0]
-
-    for n in wtfconfig.mps:
-        n.shutdown()
-        n.init()
-        n.start()
+    g_ifs = [ STA.iface[0] for STA in sta ]
 
 
 def tearDown(self):
@@ -56,23 +46,33 @@ def tearDown(self):
     if len(path_heal_time) > 0:
         print "Path healed in:        " + str(path_heal_time["path_heal"]) + " seconds"
 
+def init_nodes(n=3):
+    """Start up only number of nodes desired."""
+    for i in range(n):
+        wtfconfig.mps[i].init()
+        wtfconfig.mps[i].start()
+
 
 class SimpleMeshTest(unittest.TestCase):
 
 # run before / after each test
     def setUp(self):
-        pass
+        """Shutdown all nodes before each test."""
+        for n in wtfconfig.mps:
+            n.shutdown()
 
     def tearDown(self):
         pass
 
     def test_1_throughput(self):
         fname = sys._getframe().f_code.co_name
+        # two node test
+        init_nodes(2)
 
-        dst_ip = if_2.ip
+        dst_ip = g_ifs[1].ip
 
-        perf_report = do_perf([if_1, if_2], dst_ip)
-        get_topology([if_1, if_2, if_3], fname)
+        perf_report = do_perf([g_ifs[0], g_ifs[1]], dst_ip)
+        get_topology(g_ifs[:2], fname)
         results[fname] = LinkReport(perf_report=perf_report)
         logMeasurement('tput', perf_report.tput)
         logMeasurement('loss', perf_report.loss)
@@ -84,20 +84,30 @@ class SimpleMeshTest(unittest.TestCase):
         fname = sys._getframe().f_code.co_name
 
         # do a -> b -> c
-        ifs = [if_1, if_2, if_3]
+        ifs = g_ifs[:3]
+        old_params = []
         for iface in ifs:
-            if iface != if_2:
+            if iface != g_ifs[1]:
+                old_params.append(iface.conf.mesh_params)
                 iface.conf.mesh_params = "mesh_auto_open_plinks=0"
             iface.node.reconf()
 
-        if_1.add_mesh_peer(if_2)
-        if_3.add_mesh_peer(if_2)
+        g_ifs[0].add_mesh_peer(g_ifs[1])
+        g_ifs[2].add_mesh_peer(g_ifs[1])
 
-        perf_report = do_perf([if_1, if_3], if_3.ip)
-        get_topology([if_1, if_2, if_3], fname)
+        perf_report = do_perf([g_ifs[0], g_ifs[2]], g_ifs[2].ip)
+        get_topology(g_ifs[:3], fname)
+
+        # reset mesh params
+        n = 0
+        for iface in ifs:
+            if iface != g_ifs[1]:
+                iface.conf.mesh_params = old_params[n]
+                n += 1
+
         # test multi-hop performance using a single radio for forwarding
         results[fname] = LinkReport(perf_report=perf_report)
-        if_2.dump_mesh_stats()
+        g_ifs[1].dump_mesh_stats()
         logMeasurement('tput', perf_report.tput)
         logMeasurement('loss', perf_report.loss)
         self.failIf(perf_report.tput < (exp_results["test2"]),
@@ -106,26 +116,29 @@ class SimpleMeshTest(unittest.TestCase):
 
     def test_3_path_healing(self):
         """Kill a radio, then bring back and measure path heal time."""
+        # two node test
+        init_nodes(2)
+
         count = 30
         interval = .1
         found = 0
 
         # check if ping is alive
-        ping_results = if_1.node.ping(if_2.ip, count=3).stdout
+        ping_results = g_ifs[0].node.ping(g_ifs[1].ip, count=3).stdout
         ping_results = ping_results[-2]
         self.failIf(ping_results.find("100%") != -1, "not connected on initial ping")
 
         # turn off radio and ping to make sure we drop all packets
-        if_2.set_radio(0)
-        ping_results = if_1.node.ping(if_2.ip, count=20, interval=.1, timeout=20).stdout
+        g_ifs[1].set_radio(0)
+        ping_results = g_ifs[0].node.ping(g_ifs[1].ip, count=20, interval=.1, timeout=20).stdout
         ping_results = ping_results[-2]
         self.failIf(ping_results.find("100%") == -1,
                     "still connected")
-        if_1.dump_mpaths()
+        g_ifs[0].dump_mpaths()
 
         # turn back on radio and start ping
-        if_2.set_radio(1)
-        ping_results = if_1.node.ping(if_2.ip, count=count, interval=interval).stdout
+        g_ifs[1].set_radio(1)
+        ping_results = g_ifs[0].node.ping(g_ifs[1].ip, count=count, interval=interval).stdout
         # look for first icmp_seq= and grab the request number
         for icmp in ping_results[1:]:
             if icmp.find("icmp_seq="):
@@ -137,4 +150,4 @@ class SimpleMeshTest(unittest.TestCase):
         #expects no loss after reconnected
         path_heal_time["path_heal"] = found * interval
         logMeasurement("found", path_heal_time["path_heal"])
-        get_topology([if_1, if_2, if_3], fname)
+        get_topology(g_ifs[:2], fname)
